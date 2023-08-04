bf4d3499c8b3fa695a56f406eab04c7e7916a1ff
Yannis Baltus <yannis.baltus@outlook.com>
2023-08-04 19:28:05 +0200

    Use Middlewares

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const { success } = require('./helpers/helper')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+var path = require('path')
 let pokemons = require('./mocks/mocks-pokemons')
 
 // Config
 const app = express()
 const port = 3000
 const baseApiUrl = '/api/v1'
+
+// Middleware Logger
+// app.use((req, res, next) => {
+//     console.log(`URL : ${req.url}`)
+//     next()
+// })
+
+// Middleware Morgan (Logger)
+app.use(morgan('dev'))
+
+// Favicon
+app.use(favicon(path.join(__dirname, 'public', 'icons', 'favicon.ico')))
 
 // Homepage
 app.get('/', (req, res) => {
@@ -25,6 +40,7 @@ app.get(`${baseApiUrl}/pokemons`, (req, res) => {
     const message = pokemons.length > 0 ? "The list of pokemons has been found." : "The list of pokemons has not been found."
     res.json(success(message, pokemons))
 })
+
 
 app.listen(port, () => {
     console.log(`Notre application Node est démarrée sur : http://localhost:${port}`)
